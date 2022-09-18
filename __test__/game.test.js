0729d5da8662ad6cb8b6a8b7eec8f49b763b7509
Martyna Shallenberg <martynavictoria@gmail.com>
2022-09-18 06:58:49 -0600

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 
 describe('App', () => {
   it('Contains the compiled JavaScript', async (done) => {
-    jest.setTimeout(50000);
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
